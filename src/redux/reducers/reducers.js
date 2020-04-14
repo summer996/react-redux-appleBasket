4602ea102d0ee22f74f7d82f6ee1727dfe4f2b72
summer996 <943616879@qq.com>
2020-04-14 11:36:59 +0800

    qita:格式化代码

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,61 +1,65 @@
 import { fromJS } from 'immutable';
 
 const initialState = {
-    isPicking: false,
-    apple: [
+  isPicking: false,
+  apple: [
+    {
+      id: 0,
+      weight: 233,
+      isEaten: false,
+    },
+    {
+      id: 0,
+      weight: [
         {
-            id:0,
-            weight: 233,
-            isEaten: false
+          jinzhong: 200,
         },
         {
-            id:0,
-            weight: [
-                {
-                    jinzhong: 200
-                },
-                {
-                    pizhong: 100
-                }
-            ],
-            isEaten: false
-        }
-    ],
-    apples: [
-        {
-            id:0,
-            weight: 233,
-            isEaten: false
+          pizhong: 100,
         },
-        {
-            id:1,
-            weight: 236,
-            isEaten: false
-        },
-        {
-            id:2,
-            weight: 223,
-            isEaten: false
-        }
-    ]
+      ],
+
+      isEaten: false,
+    },
+  ],
+  apples: [
+    {
+      id: 0,
+      weight: 233,
+      isEaten: false,
+    },
+    {
+      id: 1,
+      weight: 236,
+      isEaten: false,
+    },
+    {
+      id: 2,
+      weight: 223,
+      isEaten: false,
+    },
+  ],
 };
 
-console.log(fromJS(initialState))
+console.log(fromJS(initialState));
 const reducers = (state = initialState, action) => {
-    switch (action.type) {
-        case 'BEGIN_PICK_APPLE':
-            let weight = Math.floor(Math.random()*100 + 200);
-            let newApple = {
-                id: state.apples.length,
-                weight: weight,
-                isEaten: false
-            };
-            return fromJS(state).update('apples', list => list.push(newApple)).toJS();
-        case 'EAT_APPLE':
-            return fromJS(state).setIn(['apples', action.payload, 'isEaten'], true).toJS();
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'BEGIN_PICK_APPLE':
+      let weight = Math.floor(Math.random() * 100 + 200);
+      let newApple = {
+        id: state.apples.length,
+        weight: weight,
+        isEaten: false,
+      };
+      return fromJS(state)
+        .update('apples', (list) => list.push(newApple))
+        .toJS();
+    case 'EAT_APPLE':
+      return fromJS(state)
+        .setIn(['apples', action.payload, 'isEaten'], true)
+        .toJS();
+    default:
+      return state;
+  }
+};
 export default reducers;
-
